971f0c90809d717d730104d0cd5847b18d210bd8
Amelie Jule <tme871@cluster2.hpc.in.bmrc.ox.ac.uk>
2025-03-18 16:05:47 +0000

    model training first draft

---
+++ python/annot_scanvi_train.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import scanpy as sc
+import anndata as ad
+import pandas as pd
+import scvi
+
+
+############################## PARAMETERS ##############################
+
+print("Parsing arguments")
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--atlasKey", default="None", type=str,
+                    help="key of reference atlas to use for cell typing")
+parser.add_argument("--atlasTSV", default="None", type=str,
+                    help="path to the spatialhub TSV table describing reference dataset(s) to use for cell annotation")
+parser.add_argument("--featureSet", type=str,
+                    help="type of features to subset the reference atlas to before training (one of: 'hvg', 'probes' or 'both')")
+parser.add_argument("--probesMapping", type=str,
+                    help="path to the mapping file matching probe names to Ensembl IDs")
+parser.add_argument("--batchKey_hvg", type=str,
+                    help="batch key to use when re-calculating HVGs, if applicable")
+parser.add_argument("--scviNumWorkers", type=int,
+                    help="number of workers for scVI/scANVI")
+parser.add_argument("--scVI_pretrain", type=bool,
+                    help="whether to first train model using scVI")
+
+args = parser.parse_args()
+
+
+
+
+############################## SETUP ##############################
+
+# Read in metadata file
+path2meta = f"{args.atlasTSV}"
+df = pd.read_csv(path2meta, sep = "\t")
+
+# Subset to sample of interest
+df = df[df["atlas_id"] == args.atlasKey]
+assert df.shape[0] == 1, "atlas data frame can only have one single row"
+df.index = [0]
+model_key = df["atlas_id"][0]
+print(df)
+
+# Define path to pre-processed reference dataset
+# which has already been aggregated at the probe level where several genes match the same probe
+atlasPath = os.path.join("annot.dir/atlas.dir/", model_key + ".h5ad")
+
+# Read in probes mapping file
+path2mapping = f"{args.probesMapping}"
+df_map = pd.read_csv(path2mapping)
+
+
+# scVI settings
+scvi.settings.dl_num_workers = args.scviNumWorkers - 1
+#scvi.settings.num_threads = 7
+
+
+# scVI/scANVI model covariates
+scvi_batch = df['batch_key'][0]
+
+x = df['categorical_covar'][0]
+scvi_categorical = x.split(",")
+
+y = df['continuous_covar'][0]
+scvi_continuous = y.split(",")
+
+# scVI/scANVI label to use for annotation
+scvi_label = df['celltype_annot_key'][0]
+
+
+# Create out directoruis
+outDir = os.path.join("annot.dir/scanvi", model_key)
+if not os.path.exists(outDir):
+    os.mkdir(outDir)
+
+# Define model name
+x = df['categorical_covar'][0]
+x = x.replace(',', '_')
+y = df['continuous_covar'][0]
+y = y.replace(',', '_')
+
+model_name = 'BATCH_' + scvi_batch 
+if x != '':
+    model_name = model_name + '_CATEGORICAL_' + x
+if y != '':
+    model_name = model_name + '_CONTINUOUS_' + y
+
+model_name = model_name + '_ANNOT_' + scvi_label
+modelDir = os.path.join(outDir, model_name)
+if not os.path.exists(modelDir):
+    os.mkdir(modelDir)
+
+
+# Set graphical options
+plot_covar = [scvi_batch] + [df['sample_key'][0]] + [df['donor_key'][0]] + scvi_categorical + scvi_continuous
+plot_annot = [scvi_batch] + [scvi_label] + [df['lineage_key'][0]] + [df['celltype_other_key'][0]]
+
+
+
+
+############################## TASKS ##############################
+
+
+# ---------- Task 1: Load and subset reference AnnData to HVGs and/or probes ---------- #
+
+print("Importing atlas " + atlasPath)
+adata = sc.read_h5ad(atlasPath)
+
+if 'counts' in adata.layers.keys():
+  print("'counts' layer already stored in input reference dataset")
+else:  # in this case, we'll assume counts are in X slot (but worth a manual check!)
+  print("WARNING: no 'counts' layer stored in input reference dataset. Assuming adata.X slot is set to 'counts'")
+  adata.layers['counts'] = adata.X.copy()
+
+
+# Perform feature selection
+
+print("Defining feature subset to train model on.")
+
+if args.featureSet in ['hvg', 'both']:
+
+    # Retrieve HVGs from adata.var
+    if 'highly_variable' in adata.var.columns:
+        
+        if type(adata.var['highly_variable'][0]) == str:
+            adata.var['highly_variable'] = adata.var['highly_variable'] == 'True'
+        
+        hvg = adata.var.index[adata.var['highly_variable']].to_list()
+    #elif 'hvg' in adata.uns.keys():
+    #    hvg = adata.uns['hvg'].tolist()
+    else:
+        print("No HVG found in provided AnnData. Re-calculating with" + args.batchKey_hvg + "as batch_key.")
+        adata = sc.pp.highly_variable_genes(adata,
+                                            batch_key=args.batchKey_hvg,
+                                            flavor="seurat_v3_paper")
+        hvg = adata.var.index[adata.var['highly_variable']].to_list()
+        
+else:
+    hvg = []
+    
+if args.featureSet in ['probes', 'both']:
+    probes = df_map['gene_v' + df['ensembl_version'][0].astype(str)].to_list()
+else:
+    probes = []
+
+
+features = set(hvg + probes)    
+adata = adata[:, adata.var.index.isin(features)]
+print("Working with the following subset", adata)
+
+
+# ---------- Task 2: Define scANVI model, and pre-train a scVI model if applicable ---------- #
+
+# Make sure batch and categroical covariates are indeed categorical
+catVarList = [scvi_batch] + scvi_categorical
+for var in catVarList:
+    adata.obs[var] = adata.obs[var].astype('category')
+    print(adata.obs[var])
+
+# scANVI supposedly performs better when starting from model pre-trained with scVI
+# in addition, when defining scVI/scANVI model for label transfer, note the different parameters from typical SCVI recommendations - optimized for label transfer
+# https://docs.scvi-tools.org/en/stable/tutorials/notebooks/scrna/scarches_scvi_tools.html#train-reference
+
+if args.scVI_pretrain:
+
+    print("Training scVI model")
+
+    # setup AnnData for *scVI* modeling
+    scvi.model.SCVI.setup_anndata(adata, 
+                                  layer="counts", 
+                                  batch_key=scvi_batch, 
+                                  categorical_covariate_keys=scvi_categorical,
+                                  continuous_covariate_keys=scvi_continuous)
+    
+    # Define *scVI* model
+    scvi_ref = scvi.model.SCVI(
+        adata,
+        use_layer_norm="both",
+        use_batch_norm="none",
+        encode_covariates=True,
+        dropout_rate=0.2,
+        n_layers=2,
+        n_latent=10,
+        gene_likelihood='zinb'
+    )
+    
+    # Train *scVI* reference
+    scvi_ref.train(max_epochs = 50)
+    scvi_ref.save(dir_path = modelDir, overwrite = True,
+                  prefix = 'scVI_')
+    
+    # Define *scANVI* model from pre-trained *scVI* one
+    scanvi_ref = scvi.model.SCANVI.from_scvi_model(scvi_ref,
+                                                   labels_key = scvi_label,
+                                                   unlabeled_category = 'unknown',
+                                                   linear_classifier=True)  # may avoid over-fitting the training dataset
+    
+else:
+    
+    # setup AnnData directly for *scANVI* modeling
+    scvi.model.SCANVI.setup_anndata(adata,
+                                    layer="counts", 
+                                    labels_key = scvi_label, 
+                                    unlabeled_category = 'unknown',
+                                    batch_key=scvi_batch, 
+                                    categorical_covariate_keys=scvi_categorical,
+                                    continuous_covariate_keys=scvi_continuous)
+    
+    # Define *scANVI* model from scratch
+    scanvi_ref = scvi.model.SCANVI(
+        adata,
+        use_layer_norm="both",
+        use_batch_norm="none",
+        encode_covariates=True,
+        dropout_rate=0.2,
+        n_layers=2,
+        n_latent=10,
+        gene_likelihood='zinb',
+        linear_classifier=True  # may avoid over-fitting the training dataset
+    )
+
+
+# ---------- Task 3: Train scanVI model as defined above (whether from scratch or from scVI) ---------- #
+
+# For more parameter tweaking, see also: https://discourse.scverse.org/t/scvi-tools-label-transfer-accuracy/1503
+print("Training scANVI model")
+scanvi_ref.train(max_epochs=30, n_samples_per_label=100)
+
+# Save model
+if args.scVI_pretrain:
+    scanvi_ref.save(dir_path = modelDir, overwrite = True,    
+                    prefix = 'scANVI_from_scVI_')
+else:
+    scanvi_ref.save(dir_path = modelDir, overwrite = True,    
+                    prefix = 'scANVI_')
+#scanvi_ref = scvi.model.SCANVI.load(dir_path = modelDir, prefix = 'scANVI_', adata = adata)
+
+
+# Explore latent space
+print("Finding neighbours in latent space")
+adata.obsm['X_scANVI'] = scanvi_ref.get_latent_representation()
+sc.pp.neighbors(adata, use_rep='X_scANVI')
+sc.tl.umap(adata)
+adata.write_h5ad(os.path.join(modelDir, model_key + '_feature-subset.h5ad'))
+
+
+# Generate UMAP figures to assess training performance
+sc.settings.figdir = modelDir
+
+sc.pl.umap(
+    adata,
+    color=plot_covar,
+    frameon=False,
+    ncols=2,
+    save="_trained_reference_covar.pdf"
+)
+
+sc.pl.umap(
+    adata,
+    color=plot_annot,
+    frameon=False,
+    ncols=2,
+    save="_trained_reference_annot.pdf"
+)
