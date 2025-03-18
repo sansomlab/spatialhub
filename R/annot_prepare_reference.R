370819756108c8c2ab4cddb66773b04d75fc458f
Amelie Jule <tme871@cluster2.hpc.in.bmrc.ox.ac.uk>
2025-03-18 11:46:07 +0000

    initiating annotation pipeline

---
+++ R/annot_prepare_reference.R
@@ -0,0 +1,259 @@
+#!/usr/bin/Rscript --vanilla
+
+## < Add Description and usage info >
+
+message("Pre-processing reference dataset for cell type annotation")
+timestamp()
+
+
+# ---------- Libraries ----------
+
+stopifnot(
+  require(optparse),
+  require(tidyverse),
+  require(data.table),
+  require(zellkonverter),
+  require(SingleCellExperiment)
+)
+
+
+# ---------- Options ----------
+
+option_list <- list(
+    make_option(
+        c("--atlasKey"),
+        help="key of reference dataset to pre-process"
+    ),
+    make_option(
+        c("--atlasTSV"),
+        help="path to the spatialhub TSV table describing reference dataset(s) to use for cell annotation"
+    ),
+    make_option(
+        c("--reduceMeta"),
+        help="whether to reduce the metadata to its minimal variables necessary for annotation"
+    ),
+    make_option(
+        c("--path2mapping"),
+        help="path to probe mapping metadata"
+    ),
+    make_option(
+        c("--annothubDir"),
+        help="path to directory of Ensembl annotation datasets"
+    )
+)
+
+opt <- parse_args(OptionParser(option_list=option_list))
+set.seed(123456789)
+
+cat("Running with options:\n")
+print(opt)
+
+if (!dir.exists("annot.dir/atlas.dir")){
+  dir.create("annot.dir/atlas.dir")
+}
+
+
+# ---------- Setup ----------
+
+# Read in (spatialhub) metadata file
+path2meta <- opt$atlasTSV
+df <- read.delim(path2meta, sep = "\t")
+
+# Subet to reference dataset of interest
+df <- df |> dplyr::filter(atlas_id == opt$atlasKey)
+print(df)
+
+# Define metadata variables to keep accordingly
+keepVar <- c(df$lineage_key, df$celltype_annot_key, df$celltype_other_key,
+             df$batch_key, df$categorical_covar, df$continuous_covar)
+
+
+### Read in `SingleCellExperiment` object to be processed
+
+stopifnot("file not found" = file.exists(df$path))
+print(paste0("Working on dataset ", df$atlas_id, " from ", df$source))
+
+v <- unlist(strsplit(basename(df$path), "\\."))
+file_extension <- tolower(v[length(v)])
+
+if (file_extension == "h5ad") {
+
+  sce <- zellkonverter::readH5AD(df$path, verbose = TRUE)
+
+} else if (file_extension == "rds") {
+  
+  sce <- readRDS(df$path)
+  
+  # if Seurat object, convert to SingleCellExperiment
+  if (length(grep("Seurat", class(sce))) > 0) {
+    hvg <- Seurat::VariableFeatures(sce)
+    sce <- SingleCellExperiment(assays = list(counts = sce@assays$RNA@counts),
+                                colData = sce@meta.data)
+    metadata(sce)$hvg <- hvg
+  }
+
+} else {
+  stop("Please provide a `.h5ad` or `.rds` file as input.")
+}
+
+
+# ---------- Tasks ----------
+
+### Clean up metadata (optional)
+
+coldf <- as.data.frame(colData(sce))
+
+if (opt$reduceMeta) {
+    coldf <- coldf |> dplyr::select(keepVar)
+}
+#coldf$barcode_id <- rownames(coldf)
+
+if ( all(colnames(sce) == rownames(coldf)) ) {
+  colData(sce) <- DataFrame(coldf)
+} else {
+    stop("Column mismatch!")
+}
+
+
+### Clean up gene annotations, mostly for matching with probe panel names
+
+if (df$species == "mouse") {
+    species <- "mm"
+} else if (df$species == "human") {
+    species <- "hs"
+} else {
+    stop("Species not supported!")
+}
+annotFile <- paste0(opt$annothubDir, '/', species, 
+                    "_EnsDb_v", df$ensembl_version, ".tsv")
+
+print(paste0("Using the following Ensembl database for gene mapping: ", annotFile))
+ahub <- read.delim(annotFile, sep = "\t")
+stopifnot("Key not found in provided Ensembl annotation dataset (must be one of gene, ensembl_id, entrez_id)." = df$gene_key %in% names(ahub))
+
+
+# Using *rownames* to map reference dataset genes to Ensembl database
+rowdf <- data.frame("original_name" = rownames(sce), "updated_name" = rownames(sce))
+sce_genes <- rownames(sce)
+
+idx_mismatch <- which(!(sce_genes %in% ahub[, df$gene_key]))
+n_mismatch <- length(idx_mismatch)
+print(paste0("Number of genes IDs not found in selected Ensembl database: ", n_mismatch))
+
+if (df$gene_key == "gene") {
+
+  # Handle the case of suspected 1:many mappings < gene_name.1 >
+  # where multiple transcripts with distinct ensembl_ids map to the same gene
+  for (gene in sce_genes[idx_mismatch]) {
+    bgene <- tstrsplit(gene, "\\.")[[1]]  # retrieve gene "basename" 
+    if (bgene %in% sce_genes) {
+      rowdf$updated_name[rowdf$original_name == gene] <- bgene
+    }
+  }
+
+}
+
+# Now, let's match our rownames to the ensembl database
+rowdf[, df$gene_key] <- rowdf$updated_name
+rowdf <- plyr::join(rowdf, ahub, by = df$gene_key)
+
+# Remove potential duplicates due to 1:many mappings in gene:ensembl_id, keeping only the first ensembl_id
+rowdf <- rowdf |> dplyr::arrange(ensembl_id) |>
+  dplyr::select(original_name, updated_name, gene, 
+                ensembl_id, entrez_id, gene_biotype)
+rowdf <- rowdf[!duplicated(rowdf$original_name), ]
+
+# Set updated name to current *gene* (symbol) in provided Ensembl database
+rowdf$updated_name <- rowdf$gene
+
+n_mismatch <- sum(is.na(rowdf$updated_name))
+print(paste0("Number of genes IDs that remain unmapped: ", n_mismatch))
+rowdf$updated_name[is.na(rowdf$updated_name)] <- rowdf$original_name[is.na(rowdf$updated_name)]
+
+
+# Retrieve HVG information if known
+if ("highly_variable" %in% names(rowData)) {
+  rowdf$highly_variable <- rowData$highly_variable
+  HVG_found <- TRUE
+} else if ("hvg" %in% names(sce@metadata)) {
+  rowdf$highly_variable <- 'False'
+  rowdf$highly_variable[rowdf$original_name %in% sce@metadata$hvg] <- 'True'
+  HVG_found <- TRUE
+} else {
+  print("No HVG found for this reference dataset.")
+  HVG_found <- FALSE
+}
+
+
+### Retrieve CosMx probe mapping to specified Ensembl database
+
+cosmx_df <- read.csv(opt$path2mapping)
+n_probes <- length(unique(cosmx_df$probe_name))
+
+mapping_key <- paste0("gene_v", df$ensembl_version)
+stopifnot("CosMx probe name to gene symbol mapping not supported for provided Ensembl database version." = mapping_key %in% names(cosmx_df))
+
+cosmx_df$gene <- cosmx_df[, mapping_key]
+cosmx_df <- cosmx_df |> dplyr::select(probe_name, gene)
+rowdf <- plyr::join(rowdf, cosmx_df, by = "gene")
+
+n_covered <- rowdf$probe_name[!is.na(rowdf$probe_name)] |> unique() |> length()
+print(paste0("Genes in this reference dataset cover ", n_covered, 
+             " distinct probes (out of ", n_probes, " in this panel)."))
+
+# Define grouping variable, i.e. updated name with CosMx match
+rowdf$group <- rowdf$updated_name
+rowdf$group[!is.na(rowdf$probe_name)] <- rowdf$probe_name[!is.na(rowdf$probe_name)]
+
+
+### Aggregate counts for gene_ids corresponding to the same probe name
+
+print(paste0("Reducing counts matrix from ", length(unique(rowdf$original_name)), 
+             " to ", length(unique(rowdf$group)), " distinct gene_ids"))
+
+counts <- sce@assays@data$counts
+rownames(counts) <- rowdf$group
+
+# Split original counts matrix between genes that need to be aggregated or not
+dup_genes <- rowdf$group[duplicated(rowdf$group)]
+counts0 <- counts[!(rownames(counts) %in% dup_genes), ]
+counts_aggr <- counts[rownames(counts) %in% dup_genes, ]
+
+counts_aggr <- t(sapply(by(as.matrix(counts_aggr), rownames(counts_aggr), colSums), identity))
+counts_aggr <- as(counts_aggr, "sparseMatrix")  # convert back to sparse matrix
+
+if (all(colnames(counts0) == colnames(counts_aggr))) {
+  counts <- rbind(counts0, counts_aggr)
+} else {
+  stop("Mismatch in column (cell_id) names")
+}
+dim(counts)
+
+# Update 'rowdf'
+rowdf_aggr <- data.frame(gene = rownames(counts))
+if (HVG_found) {
+  rowdf_aggr$highly_variable <- 'False'
+  
+  hvg_group <- rowdf$group[rowdf$highly_variable == 'True']
+  rowdf_aggr$highly_variable[rowdf_aggr$gene %in% hvg_group] <- 'True'
+}
+rowdf <- rowdf_aggr
+rownames(rowdf) <- rowdf$gene
+
+
+### Save updated H5AD
+
+# Store it all back into one updated sce object
+rowdf <- rowdf[match(rownames(counts), rownames(rowdf)), ]
+
+if ( all(rownames(counts) == rownames(rowdf)) & all(colnames(counts) == rownames(colData(sce))) ) {
+  sce <- SingleCellExperiment(assays = list(counts = counts),
+                              colData = colData(sce),
+                              rowData = DataFrame(rowdf))
+} else {
+    stop("Mismatch in row or column names!")
+}
+
+print("Saving processed reference dataset as .h5ad file for scANVI")
+zellkonverter::writeH5AD(sce, compression = "gzip", verbose = TRUE,
+                         file = paste0("annot.dir/atlas.dir/", df$atlas_id, ".h5ad"))
--- spatialhub/tasks/samples.py
+++ spatialhub/tasks/samples.py
@@ -21,8 +21,7 @@ from pprint import pprint
 
 # ------------------------------ utility functions -------------------------------- #
 
-def check_cols(pd_frame, req_columns_list,
-               table_name="default"):
+def check_cols(pd_frame, req_columns_list, table_name="default"):
 
     for col_name in req_columns_list:
         if col_name not in pd_frame.columns:
@@ -32,8 +31,9 @@ def check_cols(pd_frame, req_columns_list,
 
 def check_values(pd_frame, col, allowed):
     '''utility function for sanity checking columns'''
+
     if not all([x in allowed for x in pd_frame[col].values]):
-        raise ValueError("Only the following values are allowed in column '"
+        raise ValueError("Only the following values are supported in column '"
                          + col + "': " + ",".join(allowed))
 
 
@@ -45,45 +45,32 @@ class samples():
     a spatial experiment.
     '''
 
-    def __init__(self,
-                 sample_tsv = None, 
-                 library_tsv = None):
+    def __init__(self, sample_tsv = None):
 
         samples = pd.read_csv(sample_tsv, sep="\t")
         required_sample_cols = ["sample_id", "slide_id", "batch",
                                 "fov_sequence", 
                                 "fov_height", "fov_width"]
         
-        check_cols(samples, required_sample_cols,
-                   "fov2samples.tsv")
-        # < Add check for fov_sequence formatting (; and blank) >
-                
-        # < Use this when expanding pipeline to different spatial technology types >
+        if "sample_name" not in samples.columns:
+            print("'sample_name' not found: setting it to 'sample_id'")
+            samples["sample_name"] = samples["sample_id"]
         
-        #libs = pd.read_csv(library_tsv, sep="\t")
-        #required_library_cols = ["technology"]
-        
-        #check_cols(libs, required_library_cols,
-        #           "libraries.tsv")
-        
-        #self.known_technology = ["cosmx",
-        #                         "xenium"]
-        
-        #check_values(libs, "technology", 
-        #             self.known_technology)
-        
+        check_cols(samples, required_sample_cols, "samples.tsv")
+
         # Check for uniqueness of sample names per slide
         x = samples["sample_id"].astype(str) #+ "_" + samples["slide_id"].astype(str)
         if not x.is_unique:
-            raise ValueError("Repeated sample_ids, Please make sure these are unique (to identify same samples across different slides, use sample_name).")
+            raise ValueError("Repeated sample_ids, please make sure these are unique (to identify same samples across different slides, use sample_name).")
         
         samples.index = x
-        self.samples = samples.to_dict(orient="index")
         
-        #libs.sort_values(["library_id",
-        #            "technology",
-        #            "sample"], inplace=True)
+        # < Add check for fov_sequence formatting (; and blank) >
+        # < Use this when expanding pipeline to different spatial technology types >
+        
+        self.samples = samples.to_dict(orient="index")
         self.libs = samples.to_dict()
+
         
     def slide_ids(self):
         '''
@@ -97,16 +84,53 @@ class samples():
         '''
         return(set(self.libs["sample_id"].values()))
     
-    def write_tsv(self, group, group_key, outfile_path):
-        '''
-        Returns a mock input TSV file per group (slide, sample, batch, etc...) to iterate a task of the pipeline on
-        '''
-        group_df = pd.DataFrame({group: group_key}, index = [0])
-        group_df.to_csv(outfile_path, index = False, sep = "\t")
+#    def slidexsample_name(self, sample_id):
+#        '''
+#        Extracts the slide sample names for a given sample_id
+#        '''
+#        self_subset = self[self['sample_id'] == 'sample_id']
+#        sampleName = self_subset['sample_name']
+#        slideName = self_subset['slide_id']
+#        return({'sample': sampleName, 'slide': slideName})
 
 #    def write_sample(self, outfile_path):
 #        '''
 #        Returns an input TSV file per sample_id, with matching variables to be used as input for the pipeline task
 #        '''
 #        group_df = pd.DataFrame({group: group_key}, index = [0])
-#        group_df.to_csv(outfile_path, index = False, sep = "\t")        
+#        group_df.to_csv(outfile_path, index = False, sep = "\t")
+
+
+class atlases():
+    '''
+    A class for defining the reference atlases to be used for 
+    cell type annotation.
+    '''
+
+    def __init__(self, atlas_tsv = None):
+
+        refs = pd.read_csv(atlas_tsv, sep="\t")
+        required_ref_cols = ["atlas_id", "path",
+                             "celltype_annot_key",
+                             "ensembl_version"]
+        
+        check_cols(refs, required_ref_cols, "atlas.tsv")
+
+        # Check for uniqueness of sample names per slide
+        x = refs["atlas_id"].astype(str)
+        if not x.is_unique:
+            raise ValueError("Repeated atlas_ids, please make sure these are unique.")
+        
+        refs.index = x
+        
+        check_values(refs, "ensembl_version", [93, 98, 110])
+        check_values(refs, "species", ["mouse", "human"])
+        
+        self.refs = refs.to_dict(orient="index")
+        self.libs = refs.to_dict()
+            
+    def atlas_ids(self):
+        '''
+        Returns a list of atlas_ids for the project
+        '''
+        return(set(self.libs["atlas_id"].values()))
